17a61d7dd78f79b3a3e3c572cc9497d224983c87
ajona <dodoajona@gmail.com>
2026-07-11 02:03:58 +0100

    Style the HTML.. it looking peng fr fr.. bout to start the js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const card = `
+  <div class="anime-card">
+    <div class="anime-poster" style="background-image:url('')"></div>
+    <div class="anime-info">
+      <div class="anime-title">Naruto</div>
+      <div class="anime-rating"><i class="fa-solid fa-star"></i>4.9</div>
+      <div class="anime-genres">Shonen</div>
+    </div>
+  </div>
+`
